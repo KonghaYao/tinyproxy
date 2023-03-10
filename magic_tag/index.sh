fd201131251c5d93c860cbfcd93d7d4eb9b986ca
江夏尧 <3446798488@qq.com>
2023-03-10 08:30:47 +0000

    fixed: all

--- magic_tag/index.sh
+++
@@ -1,7 +0,0 @@
-
-if [ -e /app/paddle/model/model.pdparams]; then
-  echo "文件存在"
-else
-  curl  -L https://huggingface.co/KonghaYao/MagicPrompt_SD_V1/resolve/main/v1.pdparams -o ./model/model.pdparams --create-dirs
-fi
-gunicorn run:app -c gunicorn.conf.py
